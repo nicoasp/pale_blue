4639656511036803fb9e3942c82018d1ef6a2367
nicoasp <nicolas.amaya@gmail.com>
2017-06-12 12:07:46 +0200

    finish basic setup

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 const server = require("http").createServer(app); // this is for socket.io
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3001);
 		
 app.use(express.static(__dirname));
 
 
 app.get('/', (req, res) => {
-	res.send('./index.html');
+	res.end('server');
 })
